2857ceb033267faa0dc0c576835d19114517c714
tkeiyama <thomas18thx@gmail.com>
2025-10-17 21:01:21 +0900

    format files

--- packages/hono-rest-api/src/server.ts
+++ packages/hono-rest-api/src/server.ts
@@ -1,23 +1,23 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
+import { Hono } from "hono";
+import { poweredBy } from "hono/powered-by";
 
-const app = new Hono()
-app.use('*', poweredBy())
+const app = new Hono();
+app.use("*", poweredBy());
 
-app.get("/", c => c.json({ hello: 'world!' }))
+app.get("/", c => c.json({ hello: "world!" }));
 
-app.post('/', async (c) => {
-  const body = await c.req.json()
-  return c.json(body)
-})
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  return c.json(body);
+});
 
 app.get("/u/:id", async (c) => {
-  const id = c.req.param('id')
-  return c.json({ id })
-})
+  const id = c.req.param("id");
+  return c.json({ id });
+});
 
 app.all("/all", (c) => {
-  return c.json({ hello: 'all!' })
-})
+  return c.json({ hello: "all!" });
+});
 
-export default app
+export default app;
